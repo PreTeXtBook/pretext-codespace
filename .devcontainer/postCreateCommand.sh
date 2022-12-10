25d2df426de5d6c4acdba86f6cfce89c6d1bd645
Oscar Levin <oscar.levin@unco.edu>
2022-12-10 17:37:21 +0000

    update setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing CodeChat"
-pip install CodeChat-Server
+# pip install CodeChat-Server
 
 echo "Install LaTeX"
-apt-get update && apt-get install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
+# apt-get update && apt-get install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
 
