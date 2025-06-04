893b6f57b1550695c6654b82d65ab22082e1336a
oscarlevin <oscarlevin@users.noreply.github.com>
2025-06-04 11:16:48 +0000

    update to latest

--- .devcontainer/installPretext.sh
+++ .devcontainer/installPretext.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file was automatically generated with PreTeXt 2.19.0.
+# This file was automatically generated with PreTeXt 2.19.2.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
 sudo apt-get update 
