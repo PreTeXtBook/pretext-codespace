1310d7cdca45f4d48851b13ab5e775e5aa92be2c
oscarlevin <oscarlevin@users.noreply.github.com>
2026-04-21 03:29:03 +0000

    update to latest

--- .devcontainer/installPandoc.sh
+++ .devcontainer/installPandoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file was automatically generated with PreTeXt 2.38.2.
+# This file was automatically generated with PreTeXt 2.38.3.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
 # Detect architecture and download appropriate Pandoc version
